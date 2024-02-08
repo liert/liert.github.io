c5e683cf158b84ffc1b4b35c6526f3d62d46cb70
liert <2102156704@qq.com>
2024-02-09 00:57:59 +0800

    new file:   home/index.php

---
+++ home/index.php
@@ -0,0 +1,15 @@
+<div class="sbox ">
+    <form class="sb_form hassbi" id="sb_form" target="_blank" action="https://cn.bing.com/search">
+        <div id="sb_form_c" class="sb_form_c tooltip">
+            <input id="sb_form_q" class="sb_form_q" name="q" type="search" placeholder="输入搜索词" maxLength="1000" autofocus="false" autocomplete="off" aria-labelledby="sb_form_c" aria-controls="sw_as" aria-autocomplete="both" aria-owns="sw_as" />
+        </div>
+        <input id="sb_form_go" type="submit" aria-label="Search" value tabIndex="0" />
+        <label for="sb_form_go" class="search icon tooltip" id="search_icon" tabIndex="-1">
+            <svg viewBox="0 0 25 25">
+                <path stroke="#007DAA" stroke-width="2.5" stroke-linecap="round" stroke-miterlimit="10" fill="none" d="M23.75 23.75l-9-9"></path>
+                <circle stroke="#007DAA" stroke-width="2.5" stroke-linecap="round" stroke-miterlimit="10" cx="9" cy="9" r="7.75" fill="none"></circle>
+                <path fill="none" d="M25 25h-25v-25h25z"></path>
+            </svg>
+        </label>
+    </form>
+</div>
